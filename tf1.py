181cbd026b791331db0fcd34df9a53bb44073d2f
zliao <liaozy@outlook.com>
2017-03-08 10:26:56 +0800

    mnist_softmax

--- tf1.py
+++ tf1.py
@@ -1,25 +1,33 @@
+#coding=gbk
 import tensorflow as tf
 import numpy as np
 
-# Create 100 phony x, y data points in NumPy, y = x * 0.1 + 0.3
-x_data = np.random.rand(100).astype(np.float32)
-y_data = x_data * 0.1 + 0.3
+# 使用 NumPy 生成假数据(phony data), 总共 100 个点.
+x_data = np.float32(np.random.rand(2, 100)) # 随机输入
+y_data = np.dot([0.100, 0.200], x_data) + 0.300
 
-# Try to find values for W and b that compute y_data = W * x_data + b
-# (We know that W should be 0.1 and b 0.3, but TensorFlow will
-# figure that out for us.)
-W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+# 构造一个线性模型
+#
 b = tf.Variable(tf.zeros([1]))
-y = W * x_data + b
+W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
+y = tf.matmul(W, x_data) + b
 
-# Minimize the mean squared errors.
+# 最小化方差
 loss = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
 
-# Before starting, initialize the variables.  We will 'run' this first.
+# 初始化变量
 init = tf.initialize_all_variables()
 
-# Launch the graph.
+# 启动图 (graph)
 sess = tf.Session()
 sess.run(init)
+
+# 拟合平面
+for step in xrange(0, 201):
+    sess.run(train)
+    if step % 20 == 0:
+        print step, sess.run(W), sess.run(b)
+
+# 得到最佳拟合结果 W: [[0.100  0.200]], b: [0.300]
