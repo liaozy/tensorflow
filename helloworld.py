e626b24320b2f963a21595ab873d0cdef0264c8b
zliao <liaozy@outlook.com>
2017-03-11 19:11:09 +0800

    update mnist

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
 
-hello=tf.constant("hello, tensorflow")
-sess=tf.Session()
-print sess.run(hello)
+# hello=tf.constant("hello, tensorflow")
+# sess=tf.Session()
+# print sess.run(hello)
+
+import tensorflow.models.image.cifar10.cifar10 as cifar10
+cifar10.maybe_download_and_extract()
+images, labels = cifar10.distorted_inputs()
+print images
+print labels
